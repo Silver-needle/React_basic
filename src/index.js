25efce9a11c3abe8ea29aab203fafb1dac98e725
Margarita Samoray <msamoray161@gmail.com>
2024-03-10 18:55:16 +0300

    0.1

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const el = <h1>Hello!</h1>;
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  el,
+  <App/>,
   document.getElementById('root')
 );
 
