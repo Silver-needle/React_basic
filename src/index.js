ddff05342935c076bf60ab63ab978701949cb2ce
Margarita Samoray <msamoray161@gmail.com>
2024-03-11 19:07:18 +0300

    строгий режим

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, {StrictMode} from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Header} from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App/>,
+ReactDOM.render(
+  <StrictMode>
+    <App/>
+  </StrictMode>,
   document.getElementById('root')
 );
 
